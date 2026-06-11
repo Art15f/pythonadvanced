e836674cfe01ac02c7aa9fe63e4fac18ed8b923f
Art15f <artferizaj999@gmail.com>
2026-06-11 17:40:54 +0200

    c

---
+++ lesson28/app/database.py
@@ -0,0 +1,14 @@
+import sqlite3
+
+def get_db_connection():
+    conn = sqlite3.connect():
+    conn.row_factory = sqlite3.Row
+    return conn
+
+def init_db():
+    conn = get_db_connection()
+    conn.execute('''
+    CREATE TABLE IF NOT EXISTS item (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name TEXT NOT NULL,
+    description''')
